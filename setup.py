49659d9506e01520e092febadd329e60d5989908
ashrith-07 <ashrith.7j@gmail.com>
2026-03-02 01:03:20 +0530

    chore: add setup.py and use editable install for imghdr shim

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="imghdr",
+    version="0.1",
+    py_modules=["imghdr"],
+    author="streamlit-shim",
+    description="Shim package supplying imghdr for Streamlit environment",
+    license="MIT",
+)
